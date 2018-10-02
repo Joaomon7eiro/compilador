4e9d728d4bf51a2ac61fd589d6a153ebe08b6fdb
joao monteiro <joaoagrom@gmail.com>
2018-10-02 20:44:46 -0300

    project update, finished

--- compilator.py
+++ compilator.py
@@ -2,79 +2,9 @@ import string
 import sys
 
 
-def is_digit(parameters):
-    line_number = parameters['line_number']
-    token = parameters['token']
-    char = parameters['char']
-    next_token = parameters['next_token']
-    tokens_array = parameters['tokens_array']
-    printLines = parameters['printLines']
+def split_token_from_array(string, printLines):
 
-    token += char
-
-    if next_token in language_letters:
-        print("erro")
-    elif next_token not in language_digits:
-        print_symbol(token, line_number, "constante inteira") if printLines else False
-        tokens_array.append(token)
-
-    return tokens_array, token
-
-
-def is_letter(parameters):
-    line_number = parameters['line_number']
-    char = parameters['char']
-    token = parameters['token']
-    next_token = parameters['next_token']
-    tokens_array = parameters['tokens_array']
-    printLines = parameters['printLines']
-
-    token += char
-
-    if next_token not in language_letters:
-
-        if token in language_words:
-            print_symbol(token, line_number, "palavra reservada") if printLines else False
-            tokens_array.append(token)
-            token = ''
-        else:
-            print_symbol(token, line_number, "identificador") if printLines else False
-            tokens_array.append(token)
-            token = ''
-
-    return tokens_array, token
-
-
-def is_symbol(parameters):
-    line_number = parameters['line_number']
-    char = parameters['char']
-    next_token = parameters['next_token']
-    tokens_array = parameters['tokens_array']
-    printLines = parameters['printLines']
-
-    compose_symbol = True
-
-    if next_token == "=" and char == ">":
-        token_symbol = ">="
-    elif next_token == "=" and char == "<":
-        token_symbol = "<="
-    elif next_token == "=" and char == "=":
-        token_symbol = "=="
-    elif next_token == "=" and char == "+":
-        token_symbol = "+="
-    elif next_token == "=" and char == "-":
-        token_symbol = "-="
-    else:
-        compose_symbol = False
-        token_symbol = char
-
-    print_symbol(token_symbol, line_number, "simbolo especial") if printLines else False
-    tokens_array.append(token_symbol)
-
-    return tokens_array, compose_symbol
-
-
-def split_token_from_array(text, printLines):
+    text = string
 
     tokens_array = []
 
@@ -82,7 +12,7 @@ def split_token_from_array(text, printLines):
 
     line_number = 0
 
-    in_quotation_marks = False
+    quotation_marks = False
 
     compose_symbol = False
 
@@ -106,17 +36,16 @@ def split_token_from_array(text, printLines):
                 compose_symbol = False
                 continue
 
-            if char == "\"" and in_quotation_marks:
+            if char == "\"" and quotation_marks:
                 print_symbol("\"", line_number, "simbolo especial") if printLines else False
                 tokens_array.append("\"")
-                in_quotation_marks = False
-
+                quotation_marks = False
             elif char == "\"":
                 print_symbol("\"", line_number, "simbolo especial") if printLines else False
                 tokens_array.append("\"")
-                in_quotation_marks = True
+                quotation_marks = True
 
-            if in_quotation_marks:
+            if quotation_marks:
                 continue
 
             if char_index + 1 < len(line):
@@ -124,22 +53,50 @@ def split_token_from_array(text, printLines):
             else:
                 next_token = " "
 
-            parameters = {
-                'token':        token,
-                'char':         char,
-                'next_token':   next_token,
-                'tokens_array': tokens_array,
-                'printLines':   printLines,
-                'line_number':   line_number
-            }
-
             if char in language_symbols:
-                tokens_array, compose_symbol = is_symbol(parameters)
-            elif char in language_letters:
-                tokens_array, token = is_letter(parameters)
-            elif char in language_digits:
-                tokens_array, token = is_digit(parameters)
 
+                compose_symbol = True
+
+                if next_token == "=" and char == ">":
+                    token_symbol = ">="
+                elif next_token == "=" and char == "<":
+                    token_symbol = "<="
+                elif next_token == "=" and char == "=":
+                    token_symbol = "=="
+                elif next_token == "=" and char == "+":
+                    token_symbol = "+="
+                elif next_token == "=" and char == "-":
+                    token_symbol = "-="
+                else:
+                    compose_symbol = False
+                    token_symbol = char
+
+                print_symbol(token_symbol, line_number, "simbolo especial") if printLines else False
+                tokens_array.append(token_symbol)
+            elif char in language_letters:
+
+                token += char
+
+                if next_token not in language_letters:
+
+                    if token in language_words:
+                        print_symbol(token, line_number, "palavra reservada") if printLines else False
+                        tokens_array.append(token)
+                        token = ''
+                    else:
+                        print_symbol(token, line_number, "identificador") if printLines else False
+                        tokens_array.append(token)
+                        token = ''
+
+            elif char in language_digits:
+
+                token += char
+
+                if next_token in language_letters:
+                    print("erro")
+                elif next_token not in language_digits:
+                    print_symbol(token, line_number, "constante inteira") if printLines else False
+                    tokens_array.append(token)
 
     file.close()
     return tokens_array
